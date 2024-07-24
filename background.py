1aed4fa93e8e88343fd83f1fb32c04e99694a600
ghoney <wifi9195@naver.com>
2024-07-24 15:26:46 +0900

    Update background.py

--- background.py
+++ background.py
@@ -94,7 +94,7 @@ def add_bg_from_url6():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://i.imgur.com/eH6IZeU.png?1");
+             background-image: url("https://i.imgur.com/eH6IZeU.png");
              background-attachment: fixed;
              background-size: auto
              
